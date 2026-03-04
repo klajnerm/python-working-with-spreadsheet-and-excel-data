18c8508c829efed5f23063fde84caf917b781f28
KlajnermanE <KlajnermanE@G02LUXN00389.g02.fujitsu.local>
2026-03-04 15:32:52 +0100

    Write csv file from array

--- Start/Ch1/write_csv_array.py
+++ Start/Ch1/write_csv_array.py
@@ -3,16 +3,24 @@
 # Writing CSV file from an array 
 
 import csv
+from fileinput import filename
 
 # Sample data
 data = [
-  ["Item Name", "Category", "Quantity", "Wholesale Price", "Consumer Price"],
-  ["Apple","Fruits",100,0.50,0.75],
-  ["Banana","Fruits",150,0.35,0.50],
-  ["Orange","Fruits",120,0.45,0.65],
-  ["Grapes","Fruits",80,0.60,0.85],
-  ["Strawberries","Fruits",90,1.20,1.50]
+    ["Item Name", "Category", "Quantity", "Wholesale Price", "Consumer Price"],
+    ["Apple","Fruits",100,0.50,0.75],
+    ["Banana","Fruits",150,0.35,0.50],
+    ["Orange","Fruits",120,0.45,0.65],
+    ["Grapes","Fruits",80,0.60,0.85],
+    ["Strawberries","Fruits",90,1.20,1.50]
 ]
 
 
+def write_array_to_csv(data, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
+
+        
 # Write data to CSV file
+write_array_to_csv(data, "output.csv")
