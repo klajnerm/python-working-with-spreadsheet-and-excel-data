ff56241407e71db2caefc9f35034cc83f67a3c66
KlajnermanE <KlajnermanE@G02LUXN00389.g02.fujitsu.local>
2026-03-04 14:42:58 +0100

    Read csv file into an array of arrays

--- Start/Ch1/read_csv_array.py
+++ Start/Ch1/read_csv_array.py
@@ -5,12 +5,22 @@
 # import the csv module from the standard library
 import csv
 
+
 def read_csv_to_array(filename):
-  # define the array that will hold the data
-  data = []
+    # define the array that will hold the data
+    data = []
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
 
 # Read the data into an array of arrays
 inventory_data = read_csv_to_array("Inventory.csv")
 
 # Each row in the array is itself an array of values
-
+print(f"Items: {len(inventory_data)}")
+print(inventory_data[0])
+print(inventory_data[1])
+print(inventory_data[1][0], inventory_data[1][2])
