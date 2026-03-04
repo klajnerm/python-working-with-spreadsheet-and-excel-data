3ab8b3b21b4c711fd31e7da252b93a159adcd7c7
KlajnermanE <KlajnermanE@G02LUXN00389.g02.fujitsu.local>
2026-03-04 15:10:14 +0100

    Read csv file into a dictionary

--- Start/Ch1/read_csv_dict.py
+++ Start/Ch1/read_csv_dict.py
@@ -5,12 +5,23 @@
 import csv
 import pprint
 
-def read_csv_to_dict(filename):
-  data = {}
 
-  return data
+def read_csv_to_dict(filename):
+    data = {}
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        key = 0
+        for row in reader:
+            data[key] = row
+            key += 1
+            
+    return data
+
 
 # Example usage
 inventory_data = read_csv_to_dict("Inventory.csv")
 
 # Accessing data
+pprint.pprint(inventory_data)
+pprint.pprint(inventory_data[0])
+pprint.pprint(inventory_data[1]["Consumer Price"])
