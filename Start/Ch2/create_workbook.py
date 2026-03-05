d15e69a121141f3bc4f3aff20c07201494419305
KlajnermanE <KlajnermanE@G02LUXN00389.g02.fujitsu.local>
2026-03-05 15:15:35 +0100

    Create Excel workbook with openpyxl

--- Start/Ch2/create_workbook.py
+++ Start/Ch2/create_workbook.py
@@ -5,23 +5,33 @@
 from openpyxl import Workbook
 import datetime
 import random
+from pandas.io.sas.sas_constants import column_data_length_length
 
 # Create a new workbook
-
+wb = Workbook()
 
 # Get the active worksheet and name it "First"
-
+sheet = wb.active
+sheet.title = "First"
 
 # Add some data to the new sheet
-
+sheet["A1"] = "Test Data"
+sheet["B1"] = 123.4567
+sheet["C1"] = datetime.datetime(2030, 4, 1)
 
 # Use the cell() function to fill a row with values
-
+for i in range(1, 11):
+    sheet.cell(row=5, column=i).value = random.randint(1, 50)
 
 # Create a second worksheet
-
+sheet2 = wb.create_sheet("Second")
+sheet2.cell(row=2, column=2).value = "More data"
 
 # Use the append() function to add rows to the end of the sheet
-
+sheet2.append(["One", "Two", "Three"])
+sheet2.append(["One", "Two", "Three"])
+sheet2.append(["One", "Two", "Three"])
 
 # Save the workbook - values don't update until we do this!
+wb.save("NewWorkbook.xlsx")
+print("Workbook has been created successfully!")
